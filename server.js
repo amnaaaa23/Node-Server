71f0f56ec95133d7ad0f81423adcbef9e2540888
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-04-27 14:19:13 +0500

    cors issue resolved

--- server.js
+++ server.js
@@ -14,6 +14,22 @@ const socketIO = require("socket.io")(server, {
 });
 const { callChatbotAPI } = require("./chatbot");
 
+
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  res.status(200).send();
+});
+
 app.use(cors());
 app.use(express.json()); 
 
